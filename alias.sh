d2d7939d1aa086c36d5ac96292d64f7e7629a3cf
Brightcells <brightcells@gmail.com>
2016-11-18 18:16:44 +0800

    Alias sort

---
+++ alias.sh
@@ -0,0 +1,3 @@
+# *** WARNING *** The locale specified by the environment affects sort order.
+# Set LC_ALL=C to get the traditional sort order that uses native byte values.
+alias sort='LC_ALL=C sort'
