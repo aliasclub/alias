a0012922209d114689bfd167300178cb57a19cc7
Brightcells <brightcells@gmail.com>
2017-01-06 10:43:39 +0800

    :sparkles: Alias SSH force using password

--- alias.sh
+++ alias.sh
@@ -1,3 +1,14 @@
+########
+# Sort #
+########
+
 # *** WARNING *** The locale specified by the environment affects sort order.
 # Set LC_ALL=C to get the traditional sort order that uses native byte values.
 alias sort='LC_ALL=C sort'
+
+#######
+# SSH #
+#######
+
+# Force login remote machine using password
+alias sshpw='ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no'
